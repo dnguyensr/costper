32cbfd7374db80c0ced5447b4452f45eb22d8497
Daniel <danielnguyensr@gmail.com>
2017-05-30 01:46:11 -0700

    Add model tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-    it { expect(Item.reflect_on_association(:store).macro).to eql(:belongs_to) }
+  it { expect(Item.reflect_on_association(:store).macro).to eql(:belongs_to) }
 
+  it "is valid with valid attributes" do
+    store = Store.new
+    item = Item.new(name: "name", store: store)
+    expect(item).to be_valid
+  end
+
+  it "is not valid without a name" do
+    item = Item.new(name: nil)
+    expect(item).to_not be_valid
+  end
+
+  it "is not valid without a store" do
+    item = Item.new(name: "name")
+    expect(item).to_not be_valid
+  end
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    store = Store.new(name: "name")
+    expect(store).to be_valid
+  end
+
+  it "is not valid without a name" do
+    store = Store.new(name: nil)
+    expect(store).to_not be_valid
+  end
+
+  it "is not valid with the same name" do
+    Store.create(name: "newstore")
+    store = Store.new(name: "NEWSTORE")
+    expect(store).to_not be_valid
+  end
 end
