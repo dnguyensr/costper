17cacc64ac7440c7fcf76121f8debfe1f9177b5a
Daniel <danielnguyensr@gmail.com>
2017-05-30 01:29:30 -0700

    Add tests for model associations

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it { expect(Item.reflect_on_association(:store).macro).to eql(:belongs_to) }
+
 end
--- spec/models/shopping_list_spec.rb
+++ spec/models/shopping_list_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ShoppingList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it {expect(ShoppingList.reflect_on_association(:store).macro).to eql(:belongs_to) }
+    it {expect(ShoppingList.reflect_on_association(:user).macro).to eql(:belongs_to) }
+
 end
